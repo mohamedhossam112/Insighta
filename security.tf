43f223bb566669dfcb87425931b8b70cddbe99c1
Mohamed_Hosam <130778928+mohamedhossam112@users.noreply.github.com>
2025-05-16 20:45:49 +0300

    Update security.tf

--- security.tf
+++ security.tf
@@ -16,7 +16,7 @@ resource "aws_security_group" "ec2_sg" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["197.35.82.247/32"] # خليها IP بتاعك علشان تحميها (ممكن تكتب 0.0.0.0/0 للتجربة)
+    cidr_blocks      = ["197.35.82.247/32"] 
   }
 
   egress {
